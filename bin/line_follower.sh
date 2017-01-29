210dd408d99df7c25b070055308053a48293dbbd
Paul Ambrose <pambrose@mac.com>
2017-01-29 10:54:44 -0800

    Add option for blinkt led feedback

--- bin/line_follower.sh
+++ bin/line_follower.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 export PYTHONPATH=${PYTHONPATH}:../common-robotics
-./line_follower.py -w 400 --bgr "[188, 86, 254]" --display
+./line_follower.py -w 400 --bgr "188, 86, 254" --display
