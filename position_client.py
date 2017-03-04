cfbcbcca47cfebdcf05623ab170ef05424d99b47
Paul Ambrose <pambrose@mac.com>
2017-03-04 10:15:47 -0800

    Refactor grpc start/stop

--- position_client.py
+++ position_client.py
@@ -2,7 +2,6 @@ import logging
 import socket
 import time
 from threading import Event
-from threading import Thread
 
 import grpc
 from grpc_support import GenericClient
@@ -16,7 +15,7 @@ logger = logging.getLogger(__name__)
 
 class PositionClient(GenericClient):
     def __init__(self, hostname):
-        super(PositionClient, self).__init__(hostname)
+        super(PositionClient, self).__init__(hostname, "position client")
         self.__ready = Event()
         self.__id = -1
         self.__in_focus = False
@@ -26,23 +25,10 @@ class PositionClient(GenericClient):
         self.__width = -1
         self.__middle_inc = -1
 
-    # Blocking
-    def get_position(self, timeout=None):
-        while not self.stopped:
-            if not self.__ready.wait(timeout):
-                raise TimeoutException
-            with self._lock:
-                if self.__ready.is_set() and not self.stopped:
-                    self.__ready.clear()
-                    return {"id": self.__id,
-                            "in_focus": self.__in_focus,
-                            "mid_offset": self.__mid_offset,
-                            "degrees": self.__degrees,
-                            "mid_cross": self.__mid_cross,
-                            "width": self.__width,
-                            "middle_inc": self.__middle_inc}
+    def _mark_ready(self):
+        self.__ready.set()
 
-    def read_positions(self, pause_secs=2.0):
+    def _get_values(self, pause_secs=2.0):
         channel = grpc.insecure_channel(self._hostname)
         stub = FocusLinePositionServerStub(channel)
         while not self.stopped:
@@ -57,27 +43,32 @@ class PositionClient(GenericClient):
 
             logger.info("Connected to gRPC server at {0} [{1}]".format(self._hostname, server_info.info))
             try:
-                for pos in stub.getFocusLinePositions(client_info):
-                    with self._lock:
-                        self.__id = pos.id
-                        self.__in_focus = pos.in_focus
-                        self.__mid_offset = pos.mid_offset
-                        self.__degrees = pos.degrees
-                        self.__mid_cross = pos.mid_line_cross
-                        self.__width = pos.width
-                        self.__middle_inc = pos.middle_inc
-                    self.__ready.set()
+                for val in stub.getFocusLinePositions(client_info):
+                    with self.value_lock:
+                        self.__id = val.id
+                        self.__in_focus = val.in_focus
+                        self.__mid_offset = val.mid_offset
+                        self.__degrees = val.degrees
+                        self.__mid_cross = val.mid_line_cross
+                        self.__width = val.width
+                        self.__middle_inc = val.middle_inc
+                    self._mark_ready()
             except BaseException as e:
                 logger.info("Disconnected from gRPC server at {0} [{1}]".format(self._hostname, e))
                 time.sleep(pause_secs)
 
-    def start(self):
-        logger.info("Starting position client")
-        Thread(target=self.read_positions).start()
-        return self
-
-    def stop(self):
-        if not self.stopped:
-            logger.info("Stopping position client")
-            self.stopped = True
-            self.__ready.set()
+    # Blocking
+    def get_position(self, timeout=None):
+        while not self.stopped:
+            if not self.__ready.wait(timeout):
+                raise TimeoutException
+            with self.value_lock:
+                if self.__ready.is_set() and not self.stopped:
+                    self.__ready.clear()
+                    return {"id": self.__id,
+                            "in_focus": self.__in_focus,
+                            "mid_offset": self.__mid_offset,
+                            "degrees": self.__degrees,
+                            "mid_cross": self.__mid_cross,
+                            "width": self.__width,
+                            "middle_inc": self.__middle_inc}
--- position_server.py
+++ position_server.py
@@ -1,6 +1,5 @@
 import logging
 import time
-from threading import Thread
 
 import grpc
 from concurrent import futures
@@ -20,7 +19,7 @@ class PositionServer(FocusLinePositionServerServicer, GenericServer):
 
     def registerClient(self, request, context):
         logger.info("Connected to {0} client {1} [{2}]".format(self.desc, context.peer(), request.info))
-        with self._cnt_lock:
+        with self.cnt_lock:
             self._invoke_cnt += 1
         return ServerInfo(info="Server invoke count {0}".format(self._invoke_cnt))
 
@@ -28,18 +27,10 @@ class PositionServer(FocusLinePositionServerServicer, GenericServer):
         client_info = request.info
         return self.currval_generator(context.peer())
 
-    def write_position(self, in_focus, mid_offset, degrees, mid_line_cross, width, middle_inc):
-        if not self.stopped:
-            self.set_currval(FocusLinePosition(id=self._id,
-                                               in_focus=in_focus,
-                                               mid_offset=mid_offset,
-                                               degrees=degrees,
-                                               mid_line_cross=mid_line_cross,
-                                               width=width,
-                                               middle_inc=middle_inc))
-            self._id += 1
+    def _init_values_on_start(self):
+        self.write_position(False, -1, -1, -1, -1, -1)
 
-    def _start_position_server(self):
+    def _start_server(self):
         logger.info("Starting gRPC {0} listening on {1}".format(self.desc, self._hostname))
         self._grpc_server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
         add_FocusLinePositionServerServicer_to_server(self, self._grpc_server)
@@ -53,9 +44,13 @@ class PositionServer(FocusLinePositionServerServicer, GenericServer):
         finally:
             self.stop()
 
-    def start(self):
-        logger.info("Starting {0}".format(self.desc))
-        self.write_position(False, -1, -1, -1, -1, -1)
-        Thread(target=self._start_position_server).start()
-        time.sleep(1)
-        return self
+    def write_position(self, in_focus, mid_offset, degrees, mid_line_cross, width, middle_inc):
+        if not self.stopped:
+            self.set_currval(FocusLinePosition(id=self._id,
+                                               in_focus=in_focus,
+                                               mid_offset=mid_offset,
+                                               degrees=degrees,
+                                               mid_line_cross=mid_line_cross,
+                                               width=width,
+                                               middle_inc=middle_inc))
+            self._id += 1
